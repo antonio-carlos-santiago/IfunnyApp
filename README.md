e686f73d355ed20311bf23bfe839e10cb5d05f54
Carlos Santiago <carlos.santiago013@gmail.com>
2024-06-18 20:51:10 -0400

    adicionando campo de comentario

---
+++ README.md
@@ -0,0 +1,9 @@
+# A ifunny-tools Flet app
+
+An example of a minimal Flet app.
+
+To run the app:
+
+```
+flet run [app_directory]
+```
--- main.py
+++ main.py
@@ -6,9 +6,7 @@ def main(page: ft.Page):
     
     page.bgcolor = "#18191e"
     
-    page.platform = ft.PagePlatform.ANDROID
-    
-    imagem_titulo = ft.Image(src='./assets/likeifunny.png', width=100)
+    imagem_titulo = ft.Image(src='./assets/likeifunny.png', width=150)
     
     campo_link = ft.TextField(
         text_align=ft.TextAlign.CENTER,
@@ -28,17 +26,37 @@ def main(page: ft.Page):
     funcao_select = ft.RadioGroup(
         content=ft.Row(
             controls=[
-                ft.Radio(value=0),
-                ft.Radio(value=0),
-                ft.Radio(value=0),
-                ft.Radio(value=0),
-                ft.Radio(value=0),
+                ft.Radio(value=0, active_color="amber"),
+                ft.Radio(value=1, active_color="amber"),
+                ft.Radio(value=2, active_color="amber"),
+                ft.Radio(value=3, active_color="amber"),
+                ft.Radio(value=4, active_color="amber"),
             ],
             expand=True,
-            vertical_alignment=ft.CrossAxisAlignment.END
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
             
         )
     )
+    
+    icons_funcoes = ft.Row(
+        expand=True,
+        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+        controls=[
+            ft.Image(src='./assets/like.png', width=30),
+            ft.Image(src='./assets/dislike.png', width=30),
+            ft.Image(src='./assets/republicar.png', width=30),
+            ft.Image(src='./assets/seguir.png', width=30),
+            ft.Image(src='./assets/comentar.png', width=30)
+        ]
+    )
+    
+    campo_de_comentario = ft.TextField(
+        border_color=ft.colors.AMBER,
+        cursor_color=ft.colors.AMBER,
+        height=400
+    )
+    
+    
     
     container = ft.Container(
         content=ft.Column(
@@ -52,20 +70,30 @@ def main(page: ft.Page):
                     )
                 ),
                 ft.Container(
-                    padding=ft.padding.Padding(left=20, right=20, top=10, bottom=20),
+                    padding=ft.padding.Padding(left=20, right=20, top=10, bottom=0),
                     content=ft.ResponsiveRow(
                         controls=[
-                            campo_link,
-                            funcao_select
+                            campo_link,                            
                         ]
                     )
                 ),
+                ft.Container(
+                    padding=ft.padding.Padding(left=20, right=20, top=0, bottom=20),
+                    content=ft.ResponsiveRow(
+                        controls=[
+                            funcao_select,
+                            icons_funcoes
+                        ]
+                    )
+                )
             ], 
         )
     )
         
     page.add(
-        container
+        ft.SafeArea(
+            content=container
+        )
     )
     
     
